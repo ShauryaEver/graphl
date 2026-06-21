a0fedf66debd548431281e4eda2f3d8e7688d9e5
Shaurya Bhavsar <shaurya.ever@gmail.com>
2026-06-21 13:38:19 +0530

    for blog

--- common/components/elements/Separator.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Separator = () => {
-    return (
-        <hr className='mt-4 mb-4' />
-    )
-}
--- common/constant/metadata.ts
+++
@@ -1,7 +0,0 @@
-export const METADATA = {
-    title: "Kriscommerce",
-    bTitle: " | Electon Design",
-    keywords: "kriscommerce",
-    description: "Electon Design",
-    author: "Kris Kurniawan",
-}
